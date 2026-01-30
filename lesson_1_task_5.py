be8ecc386764f43392c9de61b8d833dcdc4a7fe6
NadezdaLee <linadya1976@gmail.com>
2026-01-31 00:20:59 +0300

    python-course1 изменения

---
+++ lesson_1_task_5.py
@@ -0,0 +1,15 @@
+def print_num(num):
+    print(num, end='')  
+# end='' чтобы все цифры печатались в одной строке
+# Вызов функции 11 раз с каждой цифрой номера
+print_num(8)
+print_num(8)
+print_num(0)
+print_num(0)
+print_num(5)
+print_num(5)
+print_num(5)
+print_num(3)
+print_num(5)
+print_num(3)
+print_num(5)
