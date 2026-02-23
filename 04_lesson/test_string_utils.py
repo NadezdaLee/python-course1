6143c51ae275c800eb0e556b53f124410e6db8fd
NadezdaLee <linadya1976@gmail.com>
2026-02-23 18:10:51 +0300

    Задание 4: добавлены юниттесты для StringUtils,составлен defects.txt

---
+++ 04_lesson/test_string_utils.py
@@ -0,0 +1,65 @@
+import pytest
+from string_utils import StringUtils
+
+string_utils = StringUtils()
+
+
+@pytest.mark.positive
+@pytest.mark.parametrize("input_str, expected", [
+    ("   skypro", "skypro"),
+    ("  hello world", "hello world"),
+])
+def test_trim_positive(input_str, expected):
+    assert string_utils.trim(input_str) == expected
+
+
+@pytest.mark.negative
+@pytest.mark.parametrize("input_str, expected", [
+    ("", ""),
+    ("skypro", "skypro"),
+    ("   ", ""),
+])
+def test_trim_negative(input_str, expected):
+    assert string_utils.trim(input_str) == expected
+
+
+@pytest.mark.positive
+@pytest.mark.parametrize("input_str, symbol, expected", [
+    ("SkyPro", "S", True),
+    ("SkyPro", "Pro", True),
+])
+def test_contains_positive(input_str, symbol, expected):
+    assert string_utils.contains(input_str, symbol) == expected
+
+
+@pytest.mark.negative
+@pytest.mark.parametrize("input_str, symbol, expected", [
+    ("SkyPro", "U", False),
+    ("", "a", False),
+])
+def test_contains_negative(input_str, symbol, expected):
+
+    assert string_utils.contains(input_str, symbol) == expected
+
+
+@pytest.mark.positive
+@pytest.mark.parametrize("input_str, symbol, expected", [
+    ("SkyPro", "k", "SyPro"),
+    ("SkyPro", "Pro", "Sky"),
+])
+def test_delete_symbol_positive(input_str, symbol, expected):
+    assert string_utils.delete_symbol(input_str, symbol) == expected
+
+
+@pytest.mark.negative
+@pytest.mark.parametrize("input_str, symbol, expected", [
+    ("SkyPro", "U", "SkyPro"),
+    ("", "a", ""),
+
+])
+def test_delete_symbol_negative(input_str, symbol, expected):
+    assert string_utils.delete_symbol(input_str, symbol) == expected
+
+
+def test_contains_empty_symbol():
+    assert string_utils.contains("SkyPro", "") is False
